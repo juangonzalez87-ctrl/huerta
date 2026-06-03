a3e7270a1a464232294da77170c2757df0e18b9b
Glaxy ford <147448058+Glaxyford@users.noreply.github.com>
2026-06-02 21:13:52 -0500

    Enhance Huerto class with plant management methods
    Added methods to manage plants in the garden.

--- ROKY/src/Huerto.java
+++ ROKY/src/Huerto.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import plantas.Planta;
 
 public class Huerto {
     private ArrayList<Planta>plantas;
@@ -12,5 +13,27 @@ public class Huerto {
         this.plantas = new ArrayList<>();
     }
 
-    
+    public double getAreaHuerto() {
+        return areaHuerto;
+    }
+    public void setAreaHuerto(double areaHuerto) {
+        this.areaHuerto = areaHuerto;
+    }
+
+    public double getLitrosAgua() {
+        return litrosAgua;
+    }
+    public void setLitrosAgua(double litrosAgua) {
+        this.litrosAgua = litrosAgua;
+    }
+
+    public void añadirPlanta(Planta p){
+        plantas.add(p);
+    }
+    public void mostrarPlantas(){
+        System.out.println("Plantas: ");
+        for (Planta p : plantas) {
+            System.out.println("PLanta: "+p.getNombre());
+        }
+    }
 }
