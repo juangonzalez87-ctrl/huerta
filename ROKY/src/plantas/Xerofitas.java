b56bb70577a04123dfb075cadd0fbec569d509af
Glaxy ford <147448058+Glaxyford@users.noreply.github.com>
2026-06-03 21:20:25 -0500

    Refactor Xerofitas class constructor and methods

--- ROKY/src/plantas/Xerofitas.java
+++ ROKY/src/plantas/Xerofitas.java
@@ -1,28 +1,33 @@
-package src.plantas;
-public class Xerofitas extends Planta {
+package plantas;
+
+public class Xerofitas extends Planta{
     private boolean tallo_grueso;
     private boolean espinas;
-    Xerofitas(String nombre, float Tiempo_sin_agua, int Agua_necesaria, float crecimiento_semanal, boolean tallo_grueso, boolean espinas){
-        super(nombre, Tiempo_sin_agua, Agua_necesaria, crecimiento_semanal);
-        this.tallo_grueso=tallo_grueso;
-        this.espinas=espinas;
-
+    
+    public Xerofitas(String nombre, double tiempoSinAgua, double crecimiento_semanal, double areaSolicitada,
+            boolean tallo_grueso, boolean espinas) {
+        super(nombre, tiempoSinAgua, crecimiento_semanal, areaSolicitada);
+        this.tallo_grueso = tallo_grueso;
+        this.espinas = espinas;
     }
+
     public boolean isTallo_grueso() {
         return tallo_grueso;
     }
     public void setTallo_grueso(boolean tallo_grueso) {
         this.tallo_grueso = tallo_grueso;
     }
+    
     public boolean isEspinas() {
         return espinas;
     }
     public void setEspinas(boolean espinas) {
         this.espinas = espinas;
     }
-@Override
-public String darInfoFinal(){
-    return (super.darInfoFinal() + " Tallo grueso: "+tallo_grueso+ " Espinas: "+espinas);
     
-}
+    @Override
+    public String darInfoFinal(){
+        super.calcLitrosAguaNecesitados(areaSolicitada, 1);
+        return (super.darInfoFinal() + " Tallo grueso: "+tallo_grueso+ " Espinas: "+espinas);
+    }
 }
