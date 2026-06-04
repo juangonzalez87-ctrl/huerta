d9e77f2601cf4f242ac8dabae5049275fa68a888
juangonzalez87-ctrl <juan.gonzalez87@uptc.edu.co>
2026-06-03 20:56:06 -0500

    Update Xerofitas.java

--- ROKY/src/plantas/Xerofitas.java
+++ ROKY/src/plantas/Xerofitas.java
@@ -1,5 +1,28 @@
-package plantas;
+package src.plantas;
+public class Xerofitas extends Planta {
+    private boolean tallo_grueso;
+    private boolean espinas;
+    Xerofitas(String nombre, float Tiempo_sin_agua, int Agua_necesaria, float crecimiento_semanal, boolean tallo_grueso, boolean espinas){
+        super(nombre, Tiempo_sin_agua, Agua_necesaria, crecimiento_semanal);
+        this.tallo_grueso=tallo_grueso;
+        this.espinas=espinas;
 
-public interface Xerofitas {
+    }
+    public boolean isTallo_grueso() {
+        return tallo_grueso;
+    }
+    public void setTallo_grueso(boolean tallo_grueso) {
+        this.tallo_grueso = tallo_grueso;
+    }
+    public boolean isEspinas() {
+        return espinas;
+    }
+    public void setEspinas(boolean espinas) {
+        this.espinas = espinas;
+    }
+@Override
+public String darInfoFinal(){
+    return (super.darInfoFinal() + " Tallo grueso: "+tallo_grueso+ " Espinas: "+espinas);
     
+}
 }
