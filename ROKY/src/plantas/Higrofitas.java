5adedbfdb2989a6640070d69a958195ef5ce434c
Glaxy ford <147448058+Glaxyford@users.noreply.github.com>
2026-06-03 21:19:28 -0500

    Update Higrofitas.java

--- ROKY/src/plantas/Higrofitas.java
+++ ROKY/src/plantas/Higrofitas.java
@@ -1,20 +1,23 @@
-package src.plantas;
-public class Higrofitas extends Planta {
+package plantas;
+public class Higrofitas extends Planta{
     private boolean Hojas_grandes;
-    Higrofitas(String nombre, float Tiempo_sin_agua, int Agua_necesaria, float crecimiento_semanal, boolean Hojas_grandes){
-        super(nombre, Tiempo_sin_agua, Agua_necesaria, crecimiento_semanal);
-        this.Hojas_grandes=Hojas_grandes;
 
+    public Higrofitas(String nombre, double tiempoSinAgua, double crecimiento_semanal, double areaSolicitada,
+            boolean hojas_grandes) {
+        super(nombre, tiempoSinAgua, crecimiento_semanal, areaSolicitada);
+        Hojas_grandes = hojas_grandes;
     }
+
     public boolean isHojas_grandes() {
         return Hojas_grandes;
     }
     public void setHojas_grandes(boolean hojas_grandes) {
         Hojas_grandes = hojas_grandes;
     }
-@Override
-public String darInfoFinal(){
-    return (super.darInfoFinal() + " Hojas grandes: "+Hojas_grandes);
-    
-}
+
+    @Override
+    public String darInfoFinal(){
+        super.calcLitrosAguaNecesitados(areaSolicitada, 5);
+        return (super.darInfoFinal() + " Hojas grandes: "+Hojas_grandes);
+    }
 }
