97b6e1ad4b7d430f74109ef5847e2fb55c33bf5b
juangonzalez87-ctrl <juan.gonzalez87@uptc.edu.co>
2026-06-03 20:51:20 -0500

    Update Higrofitas.java

--- ROKY/src/plantas/Higrofitas.java
+++ ROKY/src/plantas/Higrofitas.java
@@ -1,5 +1,20 @@
-package plantas;
+package herencia.planta;
+public class Higrofitas extends Planta {
+    private boolean Hojas_grandes;
+    Higrofitas(String nombre, float Tiempo_sin_agua, int Agua_necesaria, float crecimiento_semanal, boolean Hojas_grandes){
+        super(nombre, Tiempo_sin_agua, Agua_necesaria, crecimiento_semanal);
+        this.Hojas_grandes=Hojas_grandes;
 
-public class Higrofitas {
+    }
+    public boolean isHojas_grandes() {
+        return Hojas_grandes;
+    }
+    public void setHojas_grandes(boolean hojas_grandes) {
+        Hojas_grandes = hojas_grandes;
+    }
+@Override
+public String darInfoFinal(){
+    return (super.darInfoFinal() + " Hojas grandes: "+Hojas_grandes);
     
+}
 }
