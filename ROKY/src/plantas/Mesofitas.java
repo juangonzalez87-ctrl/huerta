8ceadf06376bf07f95da7996a32b040e289f3455
juangonzalez87-ctrl <juan.gonzalez87@uptc.edu.co>
2026-06-03 21:05:13 -0500

    Update Mesofitas.java

--- ROKY/src/plantas/Mesofitas.java
+++ ROKY/src/plantas/Mesofitas.java
@@ -1,5 +1,20 @@
-package plantas;
+package src.plantas;
+public class Mesofitas extends Planta{
+    private boolean Raices_largas;
+    Mesofitas(String nombre, float Tiempo_sin_agua, int Agua_necesaria, float crecimiento_semanal, boolean Raices_largas){
+        super(nombre, Tiempo_sin_agua, Agua_necesaria, crecimiento_semanal);
+        this.Raices_largas=Raices_largas;
 
-public class Mesofitas {
+    }
+    public boolean isRaices_largas() {
+        return Raices_largas;
+    }
+    public void setRaices_largas(boolean raices_largas) {
+        Raices_largas = raices_largas;
+    } 
     
+@Override
+public String darInfoFinal(){
+    return (super.darInfoFinal() + " Raices largas: "+Raices_largas);   
+}
 }
