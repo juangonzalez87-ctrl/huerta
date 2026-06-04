1be45da7bc7145857b03aff22d9dc39726137338
Glaxy ford <147448058+Glaxyford@users.noreply.github.com>
2026-06-04 11:57:59 -0500

    Update Huerto.java

--- ROKY/src/Huerto.java
+++ ROKY/src/Huerto.java
@@ -1,51 +1,72 @@
-import java.util.ArrayList;
-import plantas.Planta;
-
-public class Huerto {
-    private ArrayList<Planta>plantas;
+package plantas;
+public abstract class Planta {
+    //Datos ingresados
+    protected String nombre;
+    protected double tiempoSinAgua;
+    protected double crecimiento_semanal;
+    protected double areaSolicitada;
+    protected double litrosAguaNecesitados;
     
-    //Capacidad
-    private double areaHuerto;
-    private double litrosAgua;
-
-    private double aguaConsumida;
-
-    public Huerto(double areaHuerto, double litrosAgua) {
-        this.areaHuerto = areaHuerto;
-        this.litrosAgua = litrosAgua;
-        this.plantas = new ArrayList<>();
+    public Planta(String nombre, double tiempoSinAgua, double crecimiento_semanal, double areaSolicitada) {
+        this.nombre = nombre;
+        this.tiempoSinAgua = tiempoSinAgua;
+        this.crecimiento_semanal = crecimiento_semanal;
+        this.areaSolicitada = areaSolicitada;
     }
 
-    public double getAreaHuerto() {
-        return areaHuerto;
+    public String getNombre() {
+        return nombre;
     }
-    public void setAreaHuerto(double areaHuerto) {
-        this.areaHuerto = areaHuerto;
-    }
-
-    public double getLitrosAgua() {
-        return litrosAgua;
-    }
-    public void setLitrosAgua(double litrosAgua) {
-        this.litrosAgua = litrosAgua;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public void añadirPlanta(Planta p) {
-        plantas.add(p);
+    public double getTiempoSinAgua() {
+        return tiempoSinAgua;
+    }
+    public void setTiempoSinAgua(double tiempoSinAgua) {
+        this.tiempoSinAgua = tiempoSinAgua;
+    }
+
+    public double getCrecimiento_semanal() {
+        return crecimiento_semanal;
+    }
+    public void setCrecimiento_semanal(double crecimiento_semanal) {
+        this.crecimiento_semanal = crecimiento_semanal;
+    }
+
+    public double getAreaSolicitada() {
+        return areaSolicitada;
+    }
+    public void setAreaSolicitada(double areaSolicitada) {
+        this.areaSolicitada = areaSolicitada;
+        calcLitrosAguaNecesitados(areaSolicitada, areaSolicitada);
+    }
+
+    public double getLitrosAguaNecesitados(){
+        return litrosAguaNecesitados;
+    }
+
+    public void calcLitrosAguaNecesitados(double areaSolicitada, double litrosAprox) {
+        litrosAguaNecesitados = this.areaSolicitada*litrosAprox;
+    }
+
+    public String cumple(boolean c) {
+        return c ? "si":"no";
+    }
+
+    public double crecimientoEstimado(int semanas) {
+        return crecimiento_semanal * semanas;
     }
     
-    public void mostrarPlantas() {
-        System.out.println("Plantas: ");
-        for (Planta p : plantas) {
-            System.out.println("PLanta: "+p.getNombre());
-        }
+    public boolean necesitaRiego(double diasDesdeUltimoRiego) {
+        return diasDesdeUltimoRiego >= tiempoSinAgua;
+    }
+    
+    public String infoBasica() {
+        return "El agua necesaria es " + litrosAguaNecesitados + "L en " + areaSolicitada +
+               "m^2,\n tolera " + tiempoSinAgua + " días sin agua \ncrece " +
+               crecimiento_semanal + " cm/semana";
     }
 
-    public void mostrarNecesidadRiego(double diasDesdeUltimoRiego) {
-        for (Planta p : plantas) {
-            if (p.necesitaRiego(diasDesdeUltimoRiego)) {
-                System.out.println(p.getNombre() + " necesita riego.");
-            }
-        }
-    }
 }
