412afd0f68f4e2d3481518340182daa6a73a01e1
Glaxy ford <147448058+Glaxyford@users.noreply.github.com>
2026-06-04 23:13:09 -0500

    Update Gestion.java

--- ROKY/src/Gestion.java
+++ ROKY/src/Gestion.java
@@ -1,11 +1,35 @@
-import java.util.ArrayList;;
+import java.util.ArrayList;
+import java.util.Scanner;
+import plantas.Planta;
 
 public class Gestion {
+    Scanner sc = new Scanner(System.in); 
+
     private ArrayList<Huerto>huertos;
+    private Huerto huertoTrabajado;
+    private Planta plantaTrabajada;
 
     public Gestion(ArrayList<Huerto> huertos) {
         this.huertos = huertos;
     }
 
-    
+    public void mostrarHuertos(){
+        for (Huerto h : huertos) {
+            System.out.println("+-> "+ h.getNombre() +" <-");
+        }
+    }
+
+    public Huerto getHuertoTrabajado() {
+        return huertoTrabajado;
+    }
+    public void setHuertoTrabajado(Huerto huertoTrabajado) {
+        this.huertoTrabajado = huertoTrabajado;
+    }
+
+    public Planta getPlantaTrabajada() {
+        return plantaTrabajada;
+    }
+    public void setPlantaTrabajada(Planta plantaTrabajada) {
+        this.plantaTrabajada = plantaTrabajada;
+    }
 }   
